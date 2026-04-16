c88beb782d044f7542fe5ae5b1ece6dcfb3bcb1b
HudaaKhann <hudaa.woohoo@Gmail.com>
2026-04-16 16:07:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
-const tl = gsap.timeline({ paused: true });
+const openBtn = document.getElementById("openBtn");
+const card = document.getElementById("card");
 
-tl.to(".seal", {
-  scale: 0,
-  duration: 0.4,
-})
-
-.to(".flap", {
-  rotateX: -180,
-  duration: 1,
-  ease: "power2.inOut"
-}, "-=0.2")
-
-.to(".card", {
-  top: "-60px",
-  duration: 1.2,
-  ease: "power3.out"
-}, "-=0.3");
-
-
-document.getElementById("envelope").addEventListener("click", () => {
-  tl.play();
+openBtn.addEventListener("click", () => {
+  card.classList.add("show");
+  openBtn.style.display = "none";
 });
