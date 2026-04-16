635145cea74ba18107950a03abf3a8291cb6f831
HudaaKhann <hudaa.woohoo@Gmail.com>
2026-04-16 11:26:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,24 @@
-// Curtain animation
-window.onload = () => {
-  setTimeout(() => {
-    document.querySelector('.left').style.transform = "translateX(-100%)";
-    document.querySelector('.right').style.transform = "translateX(100%)";
+function openInvite() {
 
-    document.querySelectorAll('.curtain').forEach(el => {
-      el.style.transition = "1.5s ease";
-    });
-  }, 500);
-
-  // Fade animation
-  setTimeout(() => {
-    document.querySelectorAll('.fade').forEach(el => {
-      el.classList.add('show');
-    });
-  }, 1500);
-};
-
-// Scroll animation
-window.addEventListener("scroll", () => {
-  document.querySelectorAll('.section').forEach(sec => {
-    let top = sec.getBoundingClientRect().top;
-    if (top < window.innerHeight - 50) {
-      sec.classList.add('show');
-    }
+  // break seal
+  gsap.to(".seal", {
+    scale: 0,
+    duration: 0.5
   });
-});
 
-// Music
-function toggleMusic() {
-  const music = document.getElementById("music");
-  if (music.paused) {
-    music.play();
-  } else {
-    music.pause();
-  }
+  // open flap
+  gsap.to(".flap", {
+    rotateX: -180,
+    duration: 1,
+    delay: 0.3
+  });
+
+  // slide card up
+  gsap.to(".card", {
+    top: "10%",
+    duration: 1,
+    delay: 0.8,
+    ease: "power2.out"
+  });
+
 }
